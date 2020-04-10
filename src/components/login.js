2957259c8170fd7e6fe5131ff8f39710c2585db7
Frank Lee <somerlee.9@gmail.com>
2020-04-10 16:31:25 +0800

    added more pages

--- src/components/login.js
+++ src/components/login.js
@@ -5,9 +5,49 @@ import logo from "@/assets/logo.png";
 import { withRouter, Link } from "react-router-dom";
 
 class Login extends Component {
-  loginOnSubmit = event => {
+  state = {
+    user_id: "",
+    password: "",
+  };
+
+  loginOnSubmit = (event) => {
     event.preventDefault();
-    this.props.history.push("/teacher/home");
+    fetch("http://franklee.online:8080/pong", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": "true",
+      },
+      mode: "cors",
+      cache: "default",
+    });
+
+    // fetch("http://http://47.74.186.167:8080/user/login", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json;charset=UTF-8",
+    //   },
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     if (data.success) {
+    //       let token = data.subjects.token;
+    //       window.localStorage.setItem("token", token);
+    //     } else {
+    //       window.alert("wrong password");
+    //     }
+    //   });
+
+    // this.props.history.push("/teacher/home");
+  };
+
+  userIdOnChange = (event) => {
+    this.setState({ user_id: event.target.value });
+  };
+
+  passwordOnChange = (event) => {
+    this.setState({ password: event.target.value });
   };
 
   render() {
@@ -22,7 +62,8 @@ class Login extends Component {
               type="text"
               className="form-control"
               placeholder="用户名"
-              value=""
+              value={this.state.user_id}
+              onChange={this.userIdOnChange}
             />
           </div>
           <div className="form-group mb-4">
@@ -30,7 +71,8 @@ class Login extends Component {
               type="password"
               className="form-control"
               placeholder="密码"
-              value=""
+              value={this.state.password}
+              onChange={this.passwordOnChange}
             />
           </div>
           <div className="form-group text-center">
